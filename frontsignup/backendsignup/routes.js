2e8de9ee561c9243f4bd561395d679239cb9eadd
Achraf <achraf.rehioui.1@gmail.com>
2021-02-07 12:08:21 +0100

    adding route post

--- frontsignup/backendsignup/routes.js
+++ frontsignup/backendsignup/routes.js
@@ -4,8 +4,20 @@ const signUpTemplateCopy = require('../models/SignUpModels');
 
 router.post('/signup', (request, response) => {
     const signedUpUser = new signUpTemplateCopy({
-
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
     })
+
+    signedUpUser.save()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        }
+        )
 })
 
 module.exports = router
